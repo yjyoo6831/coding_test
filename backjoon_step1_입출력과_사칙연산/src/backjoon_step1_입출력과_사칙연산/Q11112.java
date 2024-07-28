6bbbba872f2823320a371542591b45e1bb67d012
yjyoo6831 <cenabro@naver.com>
2024-07-28 19:21:06 +0900

    240728

---
+++ backjoon_step1_입출력과_사칙연산/src/backjoon_step1_입출력과_사칙연산/Q11112.java
@@ -0,0 +1,11 @@
+package backjoon_step1_입출력과_사칙연산;
+
+public class Q11112 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hi")
+
+	}
+
+}
