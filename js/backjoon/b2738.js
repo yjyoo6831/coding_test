a4a9342ace97139bca784cc2a69db5e944a4a14b
yjyoo6831 <cenabro@naver.com>
2024-12-09 14:55:17 +0900

    coding_test-js

--- js/backjoon/b2738.js
+++ js/backjoon/b2738.js
@@ -1,12 +1,15 @@
-var [n,...input] = require('fs').readFileSync('backjoon/input.txt').toString().split('\n');
-console.log(input);
+const [n,...input] = require('fs').readFileSync(process.platform === "linux" ? '/dev/stdin' : 'backjoon/input.txt').toString().trim().split("\n");
+// console.log(n);
 const [w,h] = n.split(' ').map(Number);
-console.log(w,",",h);
-const sum='';
-for(let i=0;i<h;i+=2){
-    for(let j=0;j<w;j++){
-        console.log(i,j,w,h);
-        console.log(input[i][j]);
-        
+// console.log(input[0].split(' '));
+let sum=0;
+let total = '';
+for(let i=0;i<w;i++){
+    for(let j=0;j<h;j++){
+        // console.log(i,j);
+        sum=Number(input[i].split(' ')[j])+Number(input[i+w].split(' ')[j])
+        total+=sum+' ';
     }
+    console.log(total);
+    total='';
 }
